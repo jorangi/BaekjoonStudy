be92a6b41699aeeb7a612acbf424654ba0d64cc5
jorangi <shfl3182919@gmail.com>
2026-04-06 06:41:47 +0900

    24511번 문제풀이
    24511번은 스택, 큐, 덱 1문제의 마지막이던데,
    가장 빠르게 풀 수 있는 건 덱으로 푸는 것이다.
    
    원소가 1개인 큐 혹은 스택이 줄지어있음
    원소를 삽입시 가장 앞의 노드를 밀어내어 뒤로 보냄 이때 민 노드는 x(n-1), 밀린 노드는 xn이라고 함
    원소가 삽입되었을때 큐라면 기존 노드의 값이, 스택이라면 방금 넣은 값이 pop된다
    
    길이 M의 수열 C를 가져와서 차례대로 삽입할 것
    이전에 삽입한 결과는 남음
    
    첫 입력값은 이 줄의 길이 N
    둘째 입력값은 N 길이의 수열 A가 주어진다.
    이 때 해당 노드가 큐라면 0을, 스택이라면 1을 입력한다.
    셋째 입력값은 첫 노드의 수열을 입력한다.
    넷째 입력값은 새롭게 삽입하여 밀어내기를 할 수열의 길이 M을 정한다
    마지막은 M 길이의 수열 C를 작성한다.
    
    큐 스 스 큐
    1  2   3  4
    3
    2  4   7
    
    1번: 1번노드에 2입력, 1방출 -> 2번노드에 1입력 1방출 -> 3번노드에 1입력 1방출 -> 4번노드에 1입력 4방출
    2번: 1번노드에 4입력, 2방출 -> 2번노드에 2입력 2방출 -> 3번노드에 2입력 2방출 -> 4번노드에 2입력 1방출
    3번: 1번노드에 7입력, 4방출 -> ... 4번노드에 4입력 2방출
    
    => 패턴: 큐의 경우 다음노드로 밀림, 하지만 스택의 경우 새 입력값이 밀림
    
    큐 스 스 큐
    1  2   3  4
    3
    2  4   7
    
    1번: 1번노드에 2입력, 1방출 -> 2번노드에 1입력 1방출 -> 3번노드에 1입력 1방출 -> 4번노드에 1입력 4방출
    2번: 1번노드에 4입력, 2방출 -> 2번노드에 2입력 2방출 -> 3번노드에 2입력 2방출 -> 4번노드에 2입력 1방출
    3번: 1번노드에 7입력, 4방출 -> ... 4번노드에 4입력 2방출
    
    => 패턴: 큐의 경우 다음노드로 밀림, 하지만 스택의 경우 새 입력값이 밀림
    
    스스스스스
    12345
    3
    135
    
    => 스택만 있기 때문에, 입력값 그대로 밀림
    
    큐큐큐큐
    1234
    3
    567
    
    5123
    6512
    7651
    
    =>432
    
    라고 메모했었는데,
    가장 처음했던 시도는 시간을 초과했다.
    이중 반복문을 쓰는 건데, 사실상 출제자가 의도한 실수를 범했다고 생각한다.
    
    두 번째 시도로는 패턴에 의해 도출된 것처럼 풀었다.
    다만 큐를 반전하기 위해 stack에 넣었다가 빼는 방식을 이용했는데,
    다시 생각해보니 결국 그게 deque였다.
    
    그래서 세 번째는 deque로 풀었다.
    사실 스택 반전방식이랑 deque로 푸는 것이랑 메모리 차이는 4kb 차이났다.
    이 4kb는 아마 불필요한 스택을 생성해서 반전하는 시간때문일 것이다.
    
    근데 deque도 비슷한 방식을 사용하는지 크게 차이는 안나더라

--- 24511.cpp
+++ 24511.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <queue>
-#include <stack>
+#include <deque>
+#include <vector>
 using namespace std;
 
 int main()
@@ -8,66 +8,37 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //문제대로 구현시, 큐는 0번, 스택은 1번 할당하여 전부 집어넣음
-    //스택은 그냥 무시하면 될 것 같긴 한데 일단은 정석으로 구현해볼것
-
-    //queue와 stack을 구분하는 enum -> enum은 cast해야하니 그냥 상수로
-    //노드 길이는 최대 10만
-    // enum class QUEUE_STACK
-    // {
-    //     QUEUE=0,
-    //     STACK=1
-    // };
-    const int QUEUE = 0;
-    const int STACK = 1;
-    const int NODE_LENGTH = 100000;
-    bool arr_IsStack[NODE_LENGTH];
-    queue<int> queue_stack; //결국 큐스택은 스택인 경우 그냥 삽입안한 큐이다.
+    deque<int> queue_stack; //원형큐면 앞뒤 삽입으로 편하다
 
     //queuestack 길이
     int n;
     cin >> n;
 
+    vector<int> arr_IsStack(n);
     //큐인지 스택인지
-    for(int i = 0; i < n; i++)
-    {
-        int isStack;
-        cin >> isStack;
-        arr_IsStack[i] = isStack;
-    }
+    for(int i = 0; i < n; i++) cin >> arr_IsStack[i];
 
-    // 수열 A 입력
+    // 수열 A 입력, 스택이면 안넣는다.
+    // pop을 큐의 역순으로 한다 -> 앞으로 넣는다
     for(int i = 0; i < n; i++)
     {
         int elementA;
         cin >> elementA;
-        if(arr_IsStack[i] == STACK) continue;
-        queue_stack.push(elementA);
-    }
-    //큐를 반전해야 하니 스택에 넣었다가 뺴기
-    stack<int> temp;
-    while(!queue_stack.empty())
-    {
-        temp.push(queue_stack.front());
-        queue_stack.pop();
-    }
-    while(!temp.empty())
-    {
-        queue_stack.push(temp.top());
-        temp.pop();
+        if(arr_IsStack[i]) continue; //사실 STACK은 1이라서 이렇게 처리해도 된다
+        queue_stack.push_back(elementA);
     }
 
     //삽입할 수열 길이
     int m;
     cin >> m;
 
-    //수열 B는 그냥 큐를 넣으면서 출력한다.
+    //수열 B는 뒤로 넣는다
     for(int i = 0; i < m; i++)
     {
         int elementB;
         cin >> elementB;
-        queue_stack.push(elementB);
-        cout << queue_stack.front() << ' ';
-        queue_stack.pop();
+        queue_stack.push_front(elementB);
+        cout << queue_stack.back() << ' ';
+        queue_stack.pop_back();
     }
 }
